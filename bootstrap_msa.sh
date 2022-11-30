a5ed45900b61657c3f731d57b807839a38827e43
Luise Haeuser <luise.haeuser@web.de>
2022-11-30 15:13:27 +0100

    Work on Pavlos Algorithm New Results with Parsimony

--- bootstrap_msa.sh
+++ bootstrap_msa.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 FOLDER="data/language_alignments"
-ALIGNMENT_NAME="nj_subset_step10000000"
+ALIGNMENT_NAME="morpho_filtered_intersect_best_nj"
 ALIGNMENT_FILE=${ALIGNMENT_NAME}".phy"
 
 
 
 
 INPUT=${FOLDER}/${ALIGNMENT_FILE}
-DIR="data/bootstrapping/"
-mkdir $DIR
+mkdir ${FOLDER}/${ALIGNMENT_NAME}_bs
 PREFIX=${FOLDER}/${ALIGNMENT_NAME}_bs/bs
 MODEL="BIN+G"
 ./../tools/raxml-ng/build/bin/raxml-ng --bsmsa --msa $INPUT --model $MODEL --prefix ${PREFIX} --threads 2 --seed 2 -bs-trees 100
