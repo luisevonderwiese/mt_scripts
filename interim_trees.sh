69d15863805b21796a2c8cc7ea1751b05e052ec5
Luise Haeuser <luise.haeuser@web.de>
2022-12-12 18:28:35 +0100

    Tidy up and modify scripts for nice plots

--- interim_trees.sh
+++ interim_trees.sh
@@ -2,14 +2,17 @@
 FOLDER="data/language_alignments"
 ALIGNMENT_FILE="morpho.phy"
 INPUT=${FOLDER}/${ALIGNMENT_FILE}
-DIR="../output/raxml/MORPHOLOGICAL_INTERIM/"
+DIR="output/raxml/interim/"
 mkdir $DIR
 
 MODEL="BIN+G"
-PREFIX="${DIR}default_run"
-./../tools/raxml-ng/build/bin/raxml-ng --msa $INPUT --model $MODEL --prefix ${PREFIX} --threads 2 --seed 2
-#PREFIX="${DIR}pars_100_rand_100"
-#./../tools/raxml-ng/build/bin/raxml-ng --msa $INPUT --model $MODEL --prefix ${PREFIX} --threads 2 --seed 2 --tree pars{100},rand{100}
+#PREFIX="${DIR}test"
+#./../tools/raxml-ng/build/bin/raxml-ng --msa $INPUT --model $MODEL --prefix ${PREFIX} --threads 2 --seed 2 --tree rand{1}
+PREFIX="${DIR}pars100rand100bing"
+./../tools/raxml-ng/build/bin/raxml-ng --msa $INPUT --model $MODEL --prefix ${PREFIX} --threads 2 --seed 2 --tree pars{100},rand{100}
+MODEL="BIN"
+PREFIX="${DIR}pars100rand100bin"
+./../tools/raxml-ng/build/bin/raxml-ng --msa $INPUT --model $MODEL --prefix ${PREFIX} --threads 2 --seed 2 --tree pars{100},rand{100}
 #PREFIX="${DIR}start_cognate"
 #./../tools/raxml-ng/build/bin/raxml-ng --msa $INPUT --model $MODEL --prefix ${PREFIX} --threads 2 --seed 2 --tree ../data/trees/cognate_ie_compatible.tree
 #PREFIX="${DIR}pars{100},rand{100}_bin"
