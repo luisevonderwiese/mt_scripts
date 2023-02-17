09f880fca1a628ee48076394b9b24bfe042c83c8
Luise Haeuser <luise.haeuser@web.de>
2023-02-17 12:12:44 +0100

    Remove old code

--- old/cognate_distances.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cat  "../data/trees/cognate_ml.tree" "../data/trees/cognate_ie_compatible.tree"  > cognates.trees
-./../raxml-ng/build/bin/raxml-ng --rfdist --tree cognates.trees --prefix temp --nofiles
-rm cognates.trees
-
