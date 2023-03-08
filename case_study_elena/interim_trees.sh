7731eaf294e86bffe40c6ddca75195582904686a
Luise Haeuser <luise.haeuser@web.de>
2023-03-08 09:44:46 +0100

    Rework Case Study

--- case_study_elena/interim_trees.sh
+++ case_study_elena/interim_trees.sh
@@ -2,7 +2,7 @@
 INPUT="data/language_alignments/$1"
 MODEL="BIN+G"
 PREFIX="data/trees/$2pars100rand100bing"
-./../tools/raxml-ng/build/bin/raxml-ng --msa $INPUT --model $MODEL --prefix ${PREFIX} --threads 2 --seed 2 --tree pars{100},rand{100}
+./../../tools/raxml-ng/build/bin/raxml-ng --msa $INPUT --model $MODEL --prefix ${PREFIX} --threads 2 --seed 2 --tree pars{100},rand{100}
 MODEL="BIN"
 PREFIX="data/trees/$2pars100rand100bin"
-./../tools/raxml-ng/build/bin/raxml-ng --msa $INPUT --model $MODEL --prefix ${PREFIX} --threads 2 --seed 2 --tree pars{100},rand{100}
+./../../tools/raxml-ng/build/bin/raxml-ng --msa $INPUT --model $MODEL --prefix ${PREFIX} --threads 2 --seed 2 --tree pars{100},rand{100}
